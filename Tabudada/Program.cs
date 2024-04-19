9bda7e6145c2da15572841e29b2ac0e001fbd76a
L.Veronezzi <L.Veronezzi@DESKTOP-IQ2PR1Q>
2024-04-19 00:45:18 -0300

    Add project files.

---
+++ Tabudada/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 0, mult = 0;
+
+Console.WriteLine("Digite um número");
+number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < 11 && number >0; i++)
+{
+    mult = number * i;
+    Console.WriteLine($"{number}x{i} = {mult}");
+}
